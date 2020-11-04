39b3f0ec296792866e29752eb8c0b61a0f79fc2c
minguu <minguu42@gmail.com>
2020-11-04 15:05:02 +0900

    App.jsをクラスコンポーネントに変更

--- lesstube/src/App.js
+++ lesstube/src/App.js
@@ -1,10 +1,47 @@
-import logo from './logo.svg';
+import React from 'react'
+import Grid from '@material-ui/core/Grid';
+
+import Header from './components/Header'
+import MovieList from './components/MovieList'
+import Form from './components/Form'
 import './App.css';
 
-function App() {
-  return (
-    <h1>Hello, LessTube!</h1>
-  );
+// const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+
+class App extends React.Component {
+  state = {
+    videos: [],
+    now_videos: [],
+  }
+
+  // onSerchYoutube = (keyword) => {
+  //   const url = `https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&q=${keyword}&maxResults=3&key=${YOUTUBE_API_KEY}`;
+
+  //   axios.get(url).then(response => {
+  //     this.setState({
+  //       videos: response.data.items,
+  //     });
+  //   }).catch(() => {
+  //     console.log('通信に失敗しました');
+  //   });
+  // }
+
+  render() {
+    console.log(this.state.videos);
+    return (
+      <>
+      <Header>
+        <Grid container alignItems="center" justify="center" spacing={3}>
+          <Grid item xs={3}>
+            <Form />
+          </Grid>
+        </Grid>
+        <h1>動画一覧</h1>
+        <MovieList />
+      </Header>
+    </>
+    )
+  }
 }
 
 export default App;
