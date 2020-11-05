c3802fbb2fe4e973a935bc36f4957ce6936c88ec
doradoranu <thmjbk@gmail.com>
2020-11-05 17:24:57 +0900

    headerの変更

--- lesstube/src/components/MovieCard.js
+++ lesstube/src/components/MovieCard.js
@@ -7,6 +7,11 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { Collapse } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import IconButton from '@material-ui/core/IconButton';
+import clsx from 'clsx';
+
 
 const useStyles = makeStyles({
   root: {
@@ -20,6 +25,11 @@ const useStyles = makeStyles({
 export default function MediaCard(props) {
   const classes = useStyles();
   const url = 'https://www.youtube.com/embed/' + props.video.id.videoId;
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
   return (
     <Card className={classes.root}>
       <CardActionArea>
@@ -33,16 +43,30 @@ export default function MediaCard(props) {
           <Typography gutterBottom variant="h5" component="h2">
             {props.video.snippet.title}
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {props.video.snippet.description}
-          </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
         <Button size="small" color="primary" onClick={() => props.addNowVideos(props.video)}>
           今見るリストに追加
         </Button>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
       </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.video.snippet.description}
+          </Typography> 
+        </CardContent>
+      </Collapse>
     </Card>
   );
 }
